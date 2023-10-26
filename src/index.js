9ca314883501fda14b3588672a6408ca89798cc4
Jose Gaspar <jgasparlopez29@correo.unicordoba.edu.co>
2023-10-26 15:44:48 -0500

    cambio configuracion

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import app from "./app.js";
 import "./database.js";
 import { PORT } from "./config.js";
 import "./libs/initialSetup.js";
-import path from "path";
-
-app.use(express.static(path.join(__dirname,"public")))
 
 app.listen(PORT);
 console.log("Server on port", app.get("port"));
