c59dba190b2b890f65bc2facc4ab87ac43cca9df
Jose Gaspar <jgasparlopez29@correo.unicordoba.edu.co>
2023-10-26 14:54:10 -0500

    cambio configuracion de vercel

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import app from "./app.js";
 import "./database.js";
 import { PORT } from "./config.js";
 import "./libs/initialSetup.js";
+import path from "path";
+
+app.use(express.static(path.join(__dirname,"public")))
 
 app.listen(PORT);
 console.log("Server on port", app.get("port"));
