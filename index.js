56b117630c8cb19b69fb6e2472e9cd1132ad4be5
Jose Gaspar <jgasparlopez29@correo.unicordoba.edu.co>
2023-11-01 00:20:20 -0500

    cambio en configuracion deploy vercel

---
+++ index.js
@@ -0,0 +1,12 @@
+import app from "./src/app.js";
+import "./src/database.js";
+import { PORT } from "./src/config.js";
+import "./src/libs/initialSetup.js";
+import http from "http";
+
+const server = http.createServer(app);
+
+server.timeout = 600000; // 10 minutos
+
+server.listen(PORT);
+console.log("Server on port", app.get("port"));
